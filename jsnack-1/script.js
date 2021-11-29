ec90cd7df00d76127d69311ac17b51745680ce52
AngeloDalo <angelo251158@gmail.com>
2021-11-29 13:56:03 +0100

    palindroma

--- jsnack-1/script.js
+++ jsnack-1/script.js
@@ -3,3 +3,22 @@
  * Chiedere all’utente di inserire una parola
  * Creare una funzione per capire se la parola inserita è palindroma (prima senza funzione e poi con funzione)
  */
+
+function checkPalindromo(parola) {
+    const parolaDivisa = parola.split();
+    const parolaReverse = parolaDivisa.reverse();
+    const parolaInvertita = parolaReverse.join();
+    if (parola.toLowerCase() == parolaInvertita.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let parolaUtente = prompt("Inserire una palora");
+let check = checkPalindromo(parolaUtente);
+if (check ==  true) {
+    alert ("La parola è palindroma");
+} else {
+    alert ("La parola non è palindroma");
+}
